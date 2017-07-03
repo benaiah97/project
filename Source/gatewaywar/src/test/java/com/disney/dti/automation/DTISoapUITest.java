37e6e1e79ef3bb92bfacf85c62008832b173d478
vganda <vganda@DIN34001959.corp.capgemini.com>
2017-07-03 18:13:40 +0530

    Revert "DB properties changes and using getFailedTests()  of SoapUITestRunner"
    This reverts commit a1055218432123fbcfed7b61455336f03fce3366.

--- Source/gatewaywar/src/test/java/com/disney/dti/automation/DTISoapUITest.java
+++ Source/gatewaywar/src/test/java/com/disney/dti/automation/DTISoapUITest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
 
 /**
- * Test class for DTI-GQE SoapUI Automation
+ * Test class for SoapUI Automation
  * 
  * @author GANDV005
  *
@@ -20,15 +20,17 @@ public class DTISoapUITest {
 	 */
 	@Test
 	public void testDTISoapUI() {
+
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("com.disney.dti.automation.xmlPath");
 		String GQE_XML_PATH = resourceBundle.getString("GQE_XML_PATH");
 
 		SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
+		runner.setSettingsFile(GQE_XML_PATH);
 		runner.setProjectFile(GQE_XML_PATH);
 
 		try {
 			runner.run();
-			if (!runner.getFailedTests().isEmpty()) {
+			if (!runner.getAssertionResults().isEmpty()) {
 				throw new Exception();
 			}
 		} catch (Exception e) {
